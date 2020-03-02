f46b880722ecb6f8b89cfc2bc1423faea2ad9794
Rory Hellier <rory.hellier@googlemail.com>
2020-03-02 11:37:46 +0000

    Added password protection and tests. Passwords checked for min length and presence. Bcrypt implemented for hashes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Example User', email: 'email@email.com')
+    @user = User.new(name: 'Example User', email: 'email@email.com', password: 'password', password_confirmation: 'password')
   end
 
   test 'should be valid' do
@@ -52,4 +52,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test 'password should have the minimum length' do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
