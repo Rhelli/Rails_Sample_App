80e0644f54654d5dc8fe007dd7ec039c1729f194
Rory Hellier <rory.hellier@googlemail.com>
2020-03-09 12:10:33 +0000

    Added testing for signup_form (assert_no_difference)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,13 +1,16 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  get signup_path
-  assert_no_difference 'User.count' do
-    post user_path, params: { user: {
-      name: '',
-      email: 'user@invalid',
-      password: 'foo',
-      password_confirmation: 'bar'
-    }}
+  test 'invalid signup confirmation' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {
+        name: '',
+        email: 'user@invalid',
+        password: 'foo',
+        password_confirmation: 'bar'
+      } }
+    end
+    assert_template 'users/new'
   end
 end
