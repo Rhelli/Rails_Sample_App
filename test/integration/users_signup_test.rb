682f079cde3d62588d681451c182c39e8ba51be6
Rory Hellier <rory.hellier@googlemail.com>
2020-03-09 12:01:59 +0000

    Added error messages to the new.html.erb form for incorrect validation error viewing for the user. Adding testing for new user validation errors now.

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  get signup_path
+  assert_no_difference 'User.count' do
+    post user_path, params: { user: {
+      name: '',
+      email: 'user@invalid',
+      password: 'foo',
+      password_confirmation: 'bar'
+    }}
+  end
+end
