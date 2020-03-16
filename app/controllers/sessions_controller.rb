9321833e5817a807151ac6348b1eb7da45de907b
Rory Hellier <rory.hellier@googlemail.com>
2020-03-16 11:54:26 +0000

    Updated the session_controller to find and authenticate each user (under create) when logging a user in to create a new session_ )

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Logs in the user and redirects to show the user's show page
+    else
+      render 'new'
+      # Else shows error messages
+  end
+
+  def destroy
+  end
 end
