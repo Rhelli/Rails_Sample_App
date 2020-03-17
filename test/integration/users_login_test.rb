661c9793d1244312cc30c1d19b13cc7607693135
Rory Hellier <rory.hellier@googlemail.com>
2020-03-17 16:48:01 +0000

    Added another integration test to test signing in and logging out. Signing out / destroying the user session method created and test implemented

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -29,13 +29,34 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test 'valid signup information' do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: 'Example User',
-                                         email: 'example@example.com',
-                                         password: 'password',
-                                         password_confirmation: 'password' } }
+      post users_path, params: { user: 
+        { name: 'Example User',
+          email: 'example@example.com',
+          password: 'password',
+          password_confirmation: 'password' } }
     end
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
+  end
+
+  test 'login with valid information followed by logout' do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end
