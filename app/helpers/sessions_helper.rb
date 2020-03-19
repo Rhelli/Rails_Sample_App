6e08c6c1b52ad3ef617efa24533b81a9b6f4c675
Rory Hellier <rory.hellier@googlemail.com>
2020-03-19 12:49:39 +0000

    Updated sessions_helper to define new 'remember(user)' method to use the new remember method defined in the user model to assign session cookies to users when they log in. Modified sessions_controller to enable this action upon logging in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,10 +4,11 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # Logs out the current user
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   # Returns the current logged-in user (if any)
@@ -22,3 +23,10 @@ module SessionsHelper
     !current_user.nil?
   end
 end
+
+  # Logs out the current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
