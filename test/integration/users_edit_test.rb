b1daae77f9bdeed097cdfabc0d2e8236ffa3abfa
Rory Hellier <rory.hellier@googlemail.com>
2020-03-23 11:50:58 +0000

    Allowed nil passwords for user edit page, added successful user edit tests

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,7 +5,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:test)
   end
 
-  test 'Unsuccessful profile edit' do
+  test 'Unsuccessful edit' do
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: {
@@ -15,5 +15,24 @@ class UsersEditTest < ActionDispatch::IntegrationTest
       password_confirmation: 'bar'
     } }
     assert_template 'users/edit'
+    assert_select 'div.alert', 'The form contains 4 errors.'
+  end
+
+  test 'Successful Edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: { user: {
+      name: name,
+      email: email,
+      password: '',
+      password_confirmation: ''
+    } }
+    assert_not flash.empty?
+    assert_redirected_to @user_path
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
   end
 end
