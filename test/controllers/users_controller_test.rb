fe6d3806928c1b203a4e7926d7ca6d12dc8d8e42
Rory Hellier <rory.hellier@googlemail.com>
2020-03-23 12:45:36 +0000

    Added new second test user to fixtures. Added new correct_user? method to sessions_helper, added more testing for redirecting the user to root if they are trying to edit another user. Added before_filters to users_controller to protect the edit and update actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,45 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  def setup
+    @user = users(:test)
+    @other_user = users(:test2)
+  end
+
+  test 'should get new' do
     get signup_path
     assert_response :success
   end
 
+  test 'should redirect to edit when not logged in' do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect to update when not logged in' do
+    patch user_path(@user), params: { user: {
+      name: @user.name,
+      email: @user.email
+    } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect to edit when logged in as wrong user' do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect update when logged in as wrong user' do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: {
+      name: @user.name,
+      email: @user.email
+    } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'Unsuccessful edit' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: {
@@ -19,6 +20,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'Successful Edit' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = 'Foo Bar'
