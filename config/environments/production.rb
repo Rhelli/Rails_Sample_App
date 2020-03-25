a5003cbeab37cab6bc731a74b77f2a1337c32391
Rory Hellier <rory.hellier@googlemail.com>
2020-03-25 16:31:21 +0000

    Minor env config tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Rails_Sample_App_production"
 
   config.action_mailer.perform_caching = false
+  
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'quiet-sierra-21482.herokuapp.com/'
+  host = 'quiet-sierra-21482.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
@@ -74,6 +75,7 @@ Rails.application.configure do
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
+  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
