12f61b191ab39145d536733e966d17d811a0d3a1
Rory Hellier <rory.hellier@googlemail.com>
2020-03-25 18:30:03 +0000

    Added reminder/todo to production env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Rails_Sample_App_production"
-
+  // TODO # Check Mailer config in prodiuction environment
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
