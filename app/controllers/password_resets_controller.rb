cf2d0d12ecbf335d6fbb7aa91e2ef6c3bca3e9a3
Rory Hellier <rory.hellier@googlemail.com>
2020-03-26 12:51:52 +0000

    Defined the Password Reset Create Action
     - Added new create method in password_reset_controller
     - Modified the user model attr_accessor and added :reset_token
     - Added create_reset_digest and send_password_reset_email methods in the User model

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,19 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = 'An email has been sent with instructions on how to reset your password.'
+      redirect_to root_url
+    else
+      flash[:danger] = 'This email address does not exit in our system!'
+      render 'new'
+    end
+  end
+
   def edit
   end
 end
