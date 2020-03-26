14339402879a0f35046ed4c31c6c5a5999343ab4
Rory Hellier <rory.hellier@googlemail.com>
2020-03-26 16:49:44 +0000

    Enabled Password Update on Reset
     - Created a check_expiration method in the password_resets_controller
     - Added user_params to the passwor_resets_controller
     - Enhanced the update method in password_resets_controller with user_params
     - Added a password_reset_expired? method to the user model
     - Generated a new integration test for password resets
     - Created a hefty password_reset test for all wrong (and right) password reset form possibilities
     - Updated the create_reset_digest method to use update_columns vs update_attributes twice for less db hits
     - Added an additional test to password_resets_controller_tests to check is the token had expired
     - Added code to the PasswordResetsController update method to clear the reset_digest when password is reset for security
     - Added a test to check the reset_digest is cleared when passwords is reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -16,5 +20,44 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, 'Cannot Be Empty')
+      render 'edit'
+    elsif @user.update(user_params)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = 'Your password has been reset.'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # Before Filters
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # Confirms a valid user.
+  def valid_user
+    redirect_to root_url unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+  end
+
+  # Checks the expiration of a password reset token
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = 'This password reset link has expired. Please request a new one.'
+      redirect_to new_password_reset_url
+    end
   end
 end
